a7c79f5616cdf68bb2cea2209d719050331e6ca3
Chunyang Wang <wcysai@foxmail.com>
2021-11-10 17:54:02 +0800

    ..

--- XXII Open Cup. Grand Prix of Siberia/10.cpp
+++ XXII Open Cup. Grand Prix of Siberia/10.cpp
@@ -1,89 +1,130 @@
+#pragma GCC optimize(3)
 #include<bits/stdc++.h>
-#define MAXN 100005
-#define INF 1000000000000000000LL
-#define MOD 1000000007
+#define MAXN 2048
+#define INF 1000000000
+#define MOD 998244353
 #define F first
 #define S second
-#define N 2049
-#define mod 998244353
-#define G 3
 using namespace std;
 typedef long long ll;
 typedef pair<int,int> P;
-const double pi = acos(-1.0);
-int qpow(int x, int y) {
-	int ret = 1;
-	for(; y; y >>= 1, x = 1LL*x * x % mod) if(y & 1) ret = 1LL*ret * x % mod; 
-	return ret;
-}
-int rev[N];
-void ntt(int *a, int n, int o) {  //NTT
-	int G_inv = qpow(G, mod - 2), n_inv = qpow(n, mod - 2);
-	for(int i = 0; i < n; i ++) if(i > rev[i]) swap(a[i], a[rev[i]]);
-	for(int len = 2; len <= n; len <<= 1) {
-		int w0 = qpow((o == 1)? G : G_inv, (mod - 1) / len);
-		for(int j = 0; j < n; j += len) {
-			int wn = 1;
-			for(int p = j; p < j + (len >> 1); p ++, wn = 1LL* wn * w0 % mod) {
-				int X = a[p], Y = 1LL* wn * a[p + (len >> 1)]  % mod;
-				a[p] = (X + Y) % mod;
-				a[p + (len >> 1)] = (X - Y + mod) % mod;
-			}
-		}
-	}
-	if(o == -1) 
-		for(int i = 0; i < n; i ++) a[i] = 1LL* a[i] * n_inv % mod;
-}
-int n,m,x,y, a[N][N], b[N][N], al[N][N], bl[N][N];
-string s[N],t[N];
-int main() 
+int n,m,x,y;
+char s[MAXN][MAXN],t[MAXN][MAXN];
+int a[MAXN][MAXN],b[MAXN][MAXN],c[MAXN][MAXN];
+const int g=3; 
+int two[31];
+int dbit(int x)
 {
-    scanf("%d%d%d%d",&n,&m,&x,&y);
-    for(int i=0;i<n;i++) cin>>s[i];
-    for(int i=0;i<)
-	scanf("%d", &n);
-	for(int i = 0; i <= n; i ++) 
-		for(int j = 0; j <= n; j ++) scanf("%lld", &a[i][j]);
-	for(int i = 0; i <= n; i ++) 
-		for(int j = 0; j <= n; j ++) scanf("%lld", &b[i][j]);
-		
-	int len = 1;
-	for(;len <= n + n; len <<= 1);
-	
-	for(int i = 1; i < len; i ++) rev[i] = (rev[i >> 1] >> 1) | ((i & 1) * (len >> 1));
-	
-	for(int i = 0; i <= n; i ++) ntt(a[i], len, 1), ntt(b[i], len, 1); //先搞列
-	
-	m = n;
-	n = len;
-	for(int i = 0; i <= n; i ++)
-		for(int j = 0; j <= n; j ++) al[j][i] = a[i][j], bl[j][i] = b[i][j];//把矩阵转置一下
-	
-	for(int i = 0; i <= n; i ++) ntt(al[i], len, 1), ntt(bl[i], len, 1);//在搞行
-	
-	for(int i = 0; i <= n; i ++) {
-		for(int j = 0; j <= n; j ++) {
-			al[i][j] = al[i][j] * bl[i][j] % mod;//乘起来
-		}
-		ntt(al[i], len, -1);//先按照列IDFT回去
-		for(int j = 0; j <= n; j ++)
-			a[j][i] = al[i][j];//转置
-	}
-	
-	for(int i = 0; i < n; i ++) ntt(a[i], len, -1);//再按照行IDFT回去
-
-	for(int i = 0; i < 2 * m + 1; i ++) {//按题目要求输出即可
-		int ans = 0;
-		for(int j = 0; j < n; j ++) ans = ans ^ a[i][j];
-		printf("%lld ", ans);
-	} printf("\n");
-	for(int i = 0; i < 2 * m + 1; i ++) {
-		int ans = 0;
-		for(int j = 0; j < n; j ++) ans = ans ^ a[j][i];
-		printf("%lld ", ans);
-	} printf("\n");
-			
-		
-//	for(int i = 0; i <= n + m; i ++) printf("%lld ", a[i]);
-	return 0; 
+    while(x!=(x&-x)) x+=(x&-x);
+    return x;
+}
+int pow_mod(int a,int i)
+{
+    if(i==0) return 1;
+    int s=1;
+    while(i>0)
+     {
+         if(i&1) s=(1LL*s*a)%MOD;
+         a=(1LL*a*a)%MOD;
+         i>>=1;
+     }
+     return s;
+}
+int rev(int x,int r)  
+{  
+    int ans=0;  
+    for(int i=0;i<r;i++)  
+        if(x&(1<<i)) ans+=1<<(r-i-1);   
+    return ans;  
+}  
+void ntt(int n,int A[],int on)
+{
+    int r=0,cnt=0,t=n; 
+    while(t>1) {cnt++; t/=2;} 
+    for(;;r++) if((1<<r)==n)  break;   
+    for(int i=0;i<n;i++)  
+    {  
+        int tmp=rev(i,r);  
+        if(i<tmp) swap(A[i],A[tmp]);  
+    }  
+    for(int s=1;s<=r;s++)  
+    {  
+        int m=1<<s;  
+        int wn=pow_mod(g,(MOD-1)/m);  
+        for(int k=0;k<n;k+=m)  
+        {  
+            int w=1;  
+            for(int j=0;j<m/2;j++)  
+            {  
+                int t,u;  
+                t=1LL*w*A[k+j+m/2]%MOD;  
+                u=A[k+j];  
+                A[k+j]=(u+t);
+                if(A[k+j]>=MOD) A[k+j]-=MOD;  
+                A[k+j+m/2]=u+MOD-t;
+                if(A[k+j+m/2]>=MOD) A[k+j+m/2]-=MOD;
+                w=1LL*w*wn%MOD;  
+            }  
+        }  
+    }  
+    if(on==-1)  
+    {  
+        for(int i=1;i<n/2;i++)  
+            swap(A[i],A[n-i]);    
+        for(int i=0;i<n;i++)  
+            A[i]=1LL*A[i]*two[cnt]%MOD;  
+    }  
+}
+void transpose(int a[][MAXN])
+{
+	for(int i=0;i<2048;i++)
+		for(int j=i+1;j<2048;j++)
+			swap(a[i][j],a[j][i]);
+}
+void two_d_fft()
+{
+	for(int i=0;i<2048;i++) ntt(2048,a[i],1),ntt(2048,b[i],i);
+	transpose(a); transpose(b);
+	for(int i=0;i<2048;i++) ntt(2048,a[i],1),ntt(2048,b[i],1);
+	for(int i=0;i<2048;i++)
+		for(int j=0;j<2048;j++)
+			a[i][j]=1LL*a[i][j]*b[i][j]%MOD;
+	for(int i=0;i<2048;i++) ntt(2048,a[i],-1);
+	transpose(a);
+	for(int i=0;i<2048;i++) ntt(2048,a[i],-1);
+}
+vector<P> v;
+int main()
+{
+	for(int i=1;i<=30;i++) two[i]=pow_mod(1<<i,MOD-2); 
+	scanf("%d%d%d%d",&n,&m,&x,&y);
+	for(int i=0;i<n;i++) cin>>s[i];
+	for(int i=0;i<x;i++) cin>>t[i];
+	for(int i=0;i<n;i++)
+		for(int j=0;j<m;j++)
+			if(s[i][j]=='#') a[i][j]=1;
+	for(int i=0;i<x;i++)
+		for(int j=0;j<y;j++)
+			if(t[i][j]=='.') b[x-1-i][y-1-j]=1;
+	two_d_fft();
+	for(int i=0;i<2048;i++)
+		for(int j=0;j<2048;j++)
+			c[i][j]+=a[i][j];
+	memset(a,0,sizeof(a)); memset(b,0,sizeof(b));
+	for(int i=0;i<n;i++)
+		for(int j=0;j<m;j++)
+			if(s[i][j]=='.') a[i][j]=1;
+	for(int i=0;i<x;i++)
+		for(int j=0;j<y;j++)
+			if(t[i][j]=='#') b[x-1-i][y-1-j]=1;
+	two_d_fft();
+	for(int i=0;i<n;i++)
+		for(int j=0;j<m;j++)
+			c[i][j]+=a[i][j];
+	for(int i=0;i<=n-x;i++)
+		for(int j=0;j<=m-y;j++)
+			if(!c[x-1+i][y-1+j]) v.push_back(P(i+1,j+1));
+	printf("%d\n",(int)v.size());
+	for(auto p:v) printf("%d %d\n",p.F,p.S);
+	return 0;
 }
--- XXII Open Cup. Grand Prix of Siberia/2.cpp
+++ XXII Open Cup. Grand Prix of Siberia/2.cpp
@@ -1,5 +1,5 @@
 #include<bits/stdc++.h>
-#define MAXN 105
+#define MAXN 205
 #define INF 1000000000
 #define MOD 1000000007
 #define F first
@@ -13,51 +13,40 @@ const double PI=acos(-1.0);
 vector<P> points[MAXN];
 vector<pair<int,double> > G[1000005];
 double d[1000005];
-void dijkstra()
-{
-    priority_queue<PP,vector<PP>,greater<PP> > que;
-    fill(d,d+100*n,1e12);
-    for(int i=0;i<100;i++) d[i]=0,que.push(make_pair(0,i));
-    while(!que.empty())
-    {
-        PP p=que.top(); que.pop();
-        int v=p.second;
-        if(d[v]<p.first) continue;
-        for(int i=0;i<(int)G[v].size();i++)
-        {
-            pair<int,double> e=G[v][i];
-            if(d[e.F]>d[v]+e.S)
-            {
-                d[e.F]=d[v]+e.S;
-                que.push(P(d[e.F],e.F));
-            }
-        }
-    }
-}
 double get_dist(P x,P y)
 {
     return sqrt((x.F-y.F)*(x.F-y.F)+(x.S-y.S)*(x.S-y.S));
+}
+void go()
+{
+    fill(d,d+200*n,1e12);
+    for(int i=0;i<200;i++) d[i]=0;
+    for(int i=0;i<n-1;i++)
+        for(int j=0;j<200;j++)
+            for(int k=0;k<200;k++)
+                d[(i+1)*200+k]=min(d[(i+1)*200+k],d[i*200+j]+get_dist(points[i][j],points[i+1][k]));
 }
 int main()
 {
     scanf("%d%d",&n,&r);
     for(int i=0;i<n;i++) scanf("%d%d",&x[i],&y[i]);
+    if(n==2)
+    {
+        printf("%.10f\n",get_dist(P(x[0],y[0]),P(x[1],y[1]))-2*r);
+        return 0;
+    } 
     for(int i=0;i<n;i++)
     {
-        for(int j=0;j<100;j++)
+        for(int j=0;j<200;j++)
         {
-            double dx=x[i]+1.0*r*cos(2*PI/100*j);
-            double dy=y[i]+1.0*r*cos(2*PI/100*j);
+            double dx=x[i]+1.0*r*cos(2*PI/200*j);
+            double dy=y[i]+1.0*r*sin(2*PI/200*j);
             points[i].push_back(P(dx,dy));
         }
     }
-    for(int i=0;i<n-1;i++)
-        for(int j=0;j<100;j++)
-            for(int k=0;k<100;k++)
-                G[i*100+j].push_back(make_pair((i+1)*100+k,get_dist(points[i][j],points[i+1][k])));
-    dijkstra();
+    go();
     double ans=1e12;
-    for(int i=0;i<100;i++) ans=min(ans,d[(n-1)*100+i]);
+    for(int i=0;i<200;i++) ans=min(ans,d[(n-1)*200+i]);
     printf("%.10f\n",ans);
     return 0;
 } 
